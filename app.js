d15471a08fa8456457da38e332ceec9372e6149f
Chris Bart <44375878+ChrisDBart@users.noreply.github.com>
2019-02-09 10:39:04 -0600

    update 2

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
 function onReady() {
-    const addToDoForm = document.getElementById('addToDoForm');
-     const newToDoText = document.getElementById('newToDoText');
-     const toDoList = document.getElementById('toDoList');
+   const addToDoForm = document.getElementById('addToDoForm');
+   const newToDoText = document.getElementById('newToDoText');
+   const toDoList = document.getElementById('toDoList');
 
-     addToDoForm.addEventListener('submit', () => {
-    event.preventDefault();
+   addToDoForm.addEventListener('submit', event => {
+ event.preventDefault();
 
     // get the text
     let title = newToDoText.value;
@@ -15,26 +15,43 @@ function onReady() {
     // create a new input
     let checkbox = document.createElement('input');
 
+    let deleteBtn = document.createElement('button');
+
     // set the input's type to checkbox
     checkbox.type = "checkbox";
 
     // set the title
-   newLi.textContent = title;
+    newLi.textContent = "title";
 
    // attach the checkbox to the li
-   newLi.appendChild(checkbox);
+    newLi.appendChild(checkbox);
 
-   // attach the li to the ul
-    toDoList.appendChild(newLi);
+    // attach the li to the ul
+     toDoList.appendChild(newLi);
+
+
+
+    newLi.appendChild(deleteBtn);
+
+
 
     //empty the input
     newToDoText.value = '';
-    
-     });
 
+    deleteBtn.innerHTML = '<span>Delete<span>';
+
+    deleteBtn.addEventListener('click', event => {
+
+      newLi.remove();
+
+      deleteBtn.remove();
+
+      checkbox.remove();
+    });
+  });
 }
 
- window.onload = function() {
-   onReady();
 
- };
+window.onload = function() {
+  onReady();
+};
